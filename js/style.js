e81f1e8674ccabc7c7dcb94ab3620f2dc0458778
asmaaOsama <eng.asmaaosama@gmail.com>
2018-10-11 10:58:53 +0200

    first commit

---
+++ js/style.js
@@ -0,0 +1,16 @@
+$('.gallery-item').isotope({
+  // options
+  itemSelector: '.item',
+  layoutMode: 'fitRows'
+});
+
+$('.gallery-menu ul li').click(function(){
+	$('.gallery-menu ul li').removeClass('active');
+	$(this).addClass('active');
+
+	var selector=$(this).attr('data-filter');
+	$('.gallery-item').isotope({
+		filter:selector
+	});
+	return false;
+});
